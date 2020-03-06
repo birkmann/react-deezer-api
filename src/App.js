3e12668704a56d2cb90b1dea8f4b7a0d64db52d0
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-06 11:35:12 +0100

    files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
-  state = {
-    music: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      music: []
+    };
+  }
 
+  /*
   componentDidMount() {
     let URL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=the+doors";
     axios
@@ -23,10 +27,41 @@ class App extends React.Component {
         console.log(music);
       });
   }
+  */
+
+  //handleSubmit(e)
+  handleSubmit = e => {
+    e.preventDefault();
+
+    //let URL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=the+doors";
+    let URL =
+      "https://deezerdevs-deezer.p.rapidapi.com/search?q=" +
+      e.target.elements.query.value;
+    axios
+      .get(URL, {
+        params: {},
+        headers: {
+          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+          "x-rapidapi-key": "11ac06fc98msh86af0af3fa4e186p103fc8jsne419fad287e9"
+        }
+      })
+      .then(res => {
+        console.log(res.data.data);
+        const music = res.data.data;
+        this.setState({ music });
+      });
+  };
 
   render() {
     return (
       <div className="App">
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Song, Artist, Album" name="query" />
+            <button>Search</button>
+          </form>
+          <div id="ajax-results"></div>
+        </div>
         <ul>
           {this.state.music.map((music, index) => (
             <li key={index}>
