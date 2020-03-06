f49cc9612c82990b5c4e413943274fb6e0290108
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-06 12:21:02 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,30 +11,9 @@ class App extends React.Component {
     };
   }
 
-  /*
-  componentDidMount() {
-    let URL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=the+doors";
-    axios
-      .get(URL, {
-        params: {},
-        headers: {
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-          "x-rapidapi-key": "11ac06fc98msh86af0af3fa4e186p103fc8jsne419fad287e9"
-        }
-      })
-      .then(res => {
-        const music = res.data.data;
-        this.setState({ music });
-        console.log(music);
-      });
-  }
-  */
-
-  //handleSubmit(e)
   handleSubmit = e => {
     e.preventDefault();
 
-    //let URL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=the+doors";
     let URL =
       "https://deezerdevs-deezer.p.rapidapi.com/search?q=" +
       e.target.elements.query.value;
@@ -54,22 +34,34 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <div>
+      <div>
+        <div className="emotion"></div>
+        <div className="container">
+          <img src={logo} alt="Logo" className="logo" />
           <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Song, Artist, Album" name="query" />
-            <button>Search</button>
           </form>
-          <div id="ajax-results"></div>
+          <ul className="results">
+            {this.state.music.map((music, index) => (
+              <li key={index}>
+                <div className="left">
+                  <img src={music.album.cover_big} alt="cover" />
+                </div>
+                <div className="right">
+                  <div className="title">{music.title_short}</div>
+                  <div className="artist">
+                    <span>Artist: </span>
+                    {music.artist.name}
+                  </div>
+                  <div className="album">
+                    <span>Album: </span>
+                    {music.album.title}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul>
-          {this.state.music.map((music, index) => (
-            <li key={index}>
-              <img src={music.album.cover_big} alt="cover" />
-              <h2>{music.title}</h2>
-            </li>
-          ))}
-        </ul>
       </div>
     );
   }
